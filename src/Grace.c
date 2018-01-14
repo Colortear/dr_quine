d6a96b4c13f0f3e458ce107990313aa2ee8a44d2
colortear <wdebs91@gmail.com>
2018-01-14 10:33:30 +0000

    changed Grace.c to write to file

--- src/Grace.c
+++ src/Grace.c
@@ -1,3 +1,3 @@
 #include <stdio.h>
 #define Q(x)char *c=#x;x
-/*ze real shit*/Q(int main(){printf("#include <stdio.h>\n#define Q(x)char *c=#x;x\n/*ze real shit*/Q(%s)",c);return (0);})
+/*ze real shit*/Q(int main(){FILE *fp;fp = fopen("Grace_kid.c", "w+");fprintf(fp,"#include <stdio.h>\n#define Q(x)char *c=#x;x\n/*ze real shit*/Q(%s)",c);fclose(fp);return (0);})
